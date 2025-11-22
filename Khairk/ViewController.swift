e2ffa515e747be9c17b1ec2a884203df00f62801
Mohammed Alhalal <mdmoze@gmail.com>
2025-11-22 16:12:50 +0300

    App Logo and Colors

--- Khairk/ViewController.swift
+++ Khairk/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateLogoBounce()
+    }
+
+    func animateLogoBounce() {
+        // start smaller & invisible
+        logoImageView.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
+        logoImageView.alpha = 0.0
+
+        UIView.animate(
+            withDuration: 0.9,
+            delay: 0.1,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 0.8,
+            options: .curveEaseOut,
+            animations: {
+                // bounce to full size
+                self.logoImageView.transform = .identity
+                self.logoImageView.alpha = 1.0
+            },
+            completion: nil
+        )
     }
 
 
