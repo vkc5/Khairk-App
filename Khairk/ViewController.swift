60b6250c8714b7923c897289b670e5341fb4a117
malhalal <mdmoze@gmail.com>
2025-11-22 14:40:08 +0300

    Initial Commit

---
+++ Khairk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Khairk
+//
+//  Created by vkc5 on 22/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
