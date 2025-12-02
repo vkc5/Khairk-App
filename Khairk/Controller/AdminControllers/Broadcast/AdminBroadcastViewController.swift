3f3f636b1074e28f0070c55c209e4cde8a289ec6
batool <batool.m52020@gmail.com>
2025-12-02 18:18:49 +0300

    test

---
+++ Khairk/Controller/AdminControllers/Broadcast/AdminBroadcastViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdminBroadcastViewController.swift
+//  Khairk
+//
+//  Created by BP-36-201-18 on 02/12/2025.
+//
+
+import UIKit
+
+class AdminBroadcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
