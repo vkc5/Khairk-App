eac075c6903b6f4a75232a0c6c8852460b70b157
knack133 <batool.m52020@gmail.com>
2025-12-19 13:24:40 +0300

    Add push notification handling and NotificationCenter service

---
+++ Khairk/Model/Services/NotificationCenter.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationCenter.swift
+//  Khairk
+//
+//  Created by BP-36-201-18 on 19/12/2025.
+//
+import FirebaseFirestore
+
+class NotificationCenter {
+    private let db = Firestore.firestore()
+    
+    func save(title: String, body: String, userId: String){
+        print("new notification")
+    }
+}
