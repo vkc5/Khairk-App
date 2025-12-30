75cc713541649f6bf7d3605e4514f7bfa6dcd5db
yqasim <yousif.qasim2004@gmail.com>
2025-12-30 12:09:22 +0300

    It's time to merge

---
+++ Khairk/Controller/DonorControllers/Rewards/InfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoViewController.swift
+//  Khairk
+//
+//  Created by Yousif Qassim on 29/12/2025.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Khairk/Controller/DonorControllers/Rewards/testgameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  testgameViewController.swift
+//  Khairk
+//
+//  Created by Yousif Qassim on 28/12/2025.
+//
+
+import UIKit
+
+class testgameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
