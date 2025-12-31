c6efc9e82cfcac160886892d14b322e90da68536
Mohammed Alhalal <mdmoze@gmail.com>
2025-12-31 16:57:43 +0300

    Edit on SceneDelegate

--- Khairk/SceneDelegate.swift
+++ Khairk/SceneDelegate.swift
@@ -17,21 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let window = UIWindow(windowScene: windowScene)
-
-        let storyboard = UIStoryboard(name: "AdminNGOVerification", bundle: nil)
-//        let storyboard = UIStoryboard(name: "CollectorPickupHistory", bundle: nil)
-//        let storyboard = UIStoryboard(name: "AdminDonationHistory", bundle: nil)
-//        let storyboard = UIStoryboard(name: "AdminBroadcast", bundle: nil)
-//        let storyboard = UIStoryboard(name: "AdminNotifications", bundle: nil)
-        let initialVC = storyboard.instantiateInitialViewController()
-
-        window.rootViewController = initialVC
-        window.makeKeyAndVisible()
-
-        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
